b13766d5f9ded1f7b425638c3ea8cc89c873700e
Max ST <mstolpasov@gmail.com>
2019-03-30 17:21:30 +0300

    Init Commit

---
+++ client/settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: maxst
+# @Date:   2019-03-30 12:35:39
+# @Last Modified by:   maxst
+# @Last Modified time: 2019-03-30 12:42:40
+ENCODING = 'utf-8'
+
+SERVER = 'localhost'
+PORT = 2019
